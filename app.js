1bfffb861c8f41f6fb6c85f90673cdb491d0e88a
DavidEmanuel <david_samoila@yahoo.com>
2023-11-18 16:30:57 -0800

    GitIgnored node_modules, and added debug

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express'); //Express is like a communicator with requests and responses for the website
 const chalk = require('chalk'); //Chalk is used to color console messages for easier understanding and debugging
+const debug = require('debug')('app');
 const app = express(); //Making an express common app
 
 //This runs when the app is initialized/started
@@ -9,5 +10,5 @@ app.get('/', (req,res)=>{
 
 //App runs on port 3000 and 'listens'
 app.listen(3000, ()=>{
-    console.log('Listening on port ' + chalk.green('3000'));
+    debug.log(`Listening on port  + ${chalk.green('3000')}`);
 })
