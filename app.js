c8c46fccb3bf715d8087a2e8ab8159eb8a0c5ec5
DavidEmanuel <david_samoila@yahoo.com>
2023-11-18 16:56:49 -0800

    Added morgan

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 const express = require('express'); //Express is like a communicator with requests and responses for the website
 const chalk = require('chalk'); //Chalk is used to color console messages for easier understanding and debugging
-const debug = require('debug')('app'); //We use this so that we have 'debug' messages only when we run 'DEBUG=* node app.js'
+
+const debug = require('debug')('app'); /* We use this so that we have 'debug' messages only when we run 'DEBUG=* node app.js' 
+                                            Or we can even use 'DEBUG=app node app.js' to display only what the we told we want
+                                            to hear from the 'app'
+                                        */
+
+const morgan = require('morgan'); //Middleware. Basically it receives info from what device people visit the website
+
 const app = express(); //Making an express common app
+
+app.use(morgan('tiny')); //'combined' gives a lot of info, tiny gives less
 
 //This runs when the app is initialized/started
 app.get('/', (req,res)=>{
