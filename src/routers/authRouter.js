ecf72faa0517de953e48b67e6846932cfe6be77f
DavidEmanuel <david_samoila@yahoo.com>
2023-11-22 19:21:13 -0800

    Can now determine whether there is an account in the database or not. And now testing if can determine password correction

--- src/routers/authRouter.js
+++ src/routers/authRouter.js
@@ -8,24 +8,41 @@ const { render } = require('ejs');
 
 authRouter.route('/signUp').post((req,res)=>{
     const {username,password} = req.body; //Create User
-    connection.query(`SELECT username FROM profiles WHERE username=?`,username,(err, result)=>{
+    connection.query(`SELECT username FROM profiles WHERE username='${username}'`,(err, result, fields)=>{
         if(err)
         {
-            const sql = `INSERT INTO profiles (username,password) VALUES ?`;
-            const user = [[username,password]];
-            connection.query(sql, [user], (error, result)=>{
-                if (error)
-                {
-                    debug(error);
-                    return;
-                }
-
-                debug("User successfully added.")
-            });
+            debug(err);
             return;
+        } else if (result.length === 0) //If there's no user with this username
+        {
+
+            const sql = `INSERT INTO profiles (username,password) VALUES ?`;
+            const user = req.body;//[[username,password]];
+            connection.query(`INSERT INTO profiles (username,password) VALUES ('${username}','${password}')`, (error, result)=>{
+            if (error)
+            {
+                debug(error);
+                return;
+            }
+
+            debug("User successfully added.");
+
+            });
+
+            debug("Tried to create user");
+
+        } else if(password !== result[0].password) //If there's a wrong password
+        {
+
+            debug('Wrong password');
+
+        } else //If user can Log In
+        {
+
+            debug('Logged in successfully');
+
         }
-        debug("All good");
-        //res.send(result);
+
     });
 
     req.login(req.body, ()=>{
@@ -34,11 +51,11 @@ authRouter.route('/signUp').post((req,res)=>{
 });
 
 authRouter.route('/profile').get((req,res)=>{
-    res.json(req.user);
-    connection.query(`SELECT username FROM profiles`, (err, result)=>{
+    //res.json(req.user);
+    connection.query(`SELECT username FROM profiles WHERE username='admin'`, (err, result)=>{
         if (err)
             debug(err);
-        //res.render('sessions', result);
+        res.render('sessions', {result});
         
     });
 });
